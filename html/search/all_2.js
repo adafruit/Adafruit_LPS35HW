44995042537c28551b12db041362f53598b928cc
Travis CI <travis@travis-ci.org>
2019-05-19 02:32:49 +0000

    Deploy code docs to GitHub Pages Travis build: 6
    Commit: 63bf92de10ba1842414ce6a367b0ab8e7e902a9e

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['begin_5fi2c',['begin_I2C',['../class_adafruit___l_p_s35_h_w.html#a4413f448333fb9bf0163f04c13b34230',1,'Adafruit_LPS35HW']]],
-  ['begin_5fspi',['begin_SPI',['../class_adafruit___l_p_s35_h_w.html#ae1ff4857bf4f57d5347bb7f27144a33d',1,'Adafruit_LPS35HW']]]
+  ['begin_5fspi',['begin_SPI',['../class_adafruit___l_p_s35_h_w.html#ae1ff4857bf4f57d5347bb7f27144a33d',1,'Adafruit_LPS35HW::begin_SPI(uint8_t cs_pin, SPIClass *theSPI=&amp;SPI)'],['../class_adafruit___l_p_s35_h_w.html#a9b8011f0b921a4a64f17c428d3b81702',1,'Adafruit_LPS35HW::begin_SPI(int8_t cs_pin, int8_t sck_pin, int8_t miso_pin, int8_t mosi_pin)']]]
 ];
--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['enablehighthreshold',['enableHighThreshold',['../class_adafruit___l_p_s35_h_w.html#a4a97cb71a6bc3f945d50e51c2d43cf13',1,'Adafruit_LPS35HW']]],
-  ['enableinterrupts',['enableInterrupts',['../class_adafruit___l_p_s35_h_w.html#a32e0114eb5c705d10147ed25cb85728a',1,'Adafruit_LPS35HW']]],
+  ['enableinterrupts',['enableInterrupts',['../class_adafruit___l_p_s35_h_w.html#a45e27b9add5bc6a685c84ac6a2ef8adf',1,'Adafruit_LPS35HW']]],
   ['enablelowpass',['enableLowPass',['../class_adafruit___l_p_s35_h_w.html#acf7d5557c4142a4cb12ce091daa887cc',1,'Adafruit_LPS35HW']]],
   ['enablelowthreshold',['enableLowThreshold',['../class_adafruit___l_p_s35_h_w.html#a8f1ddb13c1cf2d04c7719e7a4a528a30',1,'Adafruit_LPS35HW']]]
 ];
